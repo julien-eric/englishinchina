9d944b823c6db6829398d33df9a37174f335f4f6
julzeric <julieneric11@gmail.com>
2017-02-28 23:00:37 -0500

    Adding Articles First Commit, make apostrophe drop in URL

--- controllers/articles.js
+++ controllers/articles.js
@@ -5,6 +5,7 @@ module.exports = {
 
     addArticle: function (user, article, callback) {
         var url = article.title.replace(/\s/g, '-');
+        url = url.replace(/'+/g, '');
         Article.create({title: article.title, url:url, user:user, pictureUrl: article.avatarUrl, content: article.content, kicker: article.kicker}, function(err, newArticle){
             imagesController.addImage({
                     type: 4,
