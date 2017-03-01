cb2876a657cb11308418832633aa3fa2cb47cc03
julzeric <julieneric11@gmail.com>
2017-03-01 08:30:27 -0500

    Adding Articles Third Commit, make urls lowercase

--- controllers/articles.js
+++ controllers/articles.js
@@ -4,8 +4,7 @@ var imagesController = require('./images');
 module.exports = {
 
     addArticle: function (user, article, callback) {
-        var url = article.title.replace(/\s/g, '-');
-        url = url.replace(/'+/g, '');
+        var url = article.title.replace(/\s/g, '-').replace(/'+/g, '').toLowerCase();
         Article.create({title: article.title, url:url, user:user, pictureUrl: article.avatarUrl, content: article.content, kicker: article.kicker}, function(err, newArticle){
             imagesController.addImage({
                     type: 4,
