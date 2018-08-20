e1b47dc4f671661a10e4196ed7a022f5af2f6904
julien-eric <julieneric11@gmail.com>
2018-08-19 21:55:11 -0400

    weird rename2

---
+++ public/javascripts/tinymce.js
@@ -0,0 +1,17 @@
+$(document).ready(() => {
+    tinymce.init({
+      selector: '#job-description-textarea',
+      menubar: false,
+      style_formats: [
+        {title: 'Header', format: 'h3'},
+      ],
+      setup(editor) {
+        editor.on('change', () => {
+          editor.save();
+        });
+      },
+      plugins: 'link paste',
+      toolbar: 'link undo redo styleselect bold italic outdent indent bullist numlist'
+    });
+  });
+  
