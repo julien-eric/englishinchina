4042542988ce84209ea4400d4d2e14b964011a57
julien-eric <julieneric11@gmail.com>
2018-12-13 00:58:33 -0500

    putting the all-in-one minified librairies to test download size

--- minifier.js
+++ minifier.js
@@ -17,31 +17,31 @@ cssMinPromise.catch(function (err) {
 });
 
 
-// // Third-party libs
-// let thirdPartyMinPromise = minify({
-//     compressor: gcc,
-//     input: './public/javascripts/libs/bkp/*.js',
-//     output: './public/javascripts/dist/slworldlibs.js'
-// });
+// Third-party libs
+let thirdPartyMinPromise = minify({
+    compressor: gcc,
+    input: './public/javascripts/libs/bkp/*.js',
+    output: './public/javascripts/dist/slworldlibs.js'
+});
 
-// thirdPartyMinPromise.then(function (min) {
-//     console.log(min);
-// });
-// thirdPartyMinPromise.catch(function (err) {
-//     console.log(err);
-// });
+thirdPartyMinPromise.then(function (min) {
+    console.log(min);
+});
+thirdPartyMinPromise.catch(function (err) {
+    console.log(err);
+});
 
 
-// // SLWORLD front-end
-// let promise = minify({
-//     compressor: gcc,
-//     input: './public/javascripts/*.js',
-//     output: './public/javascripts/dist/slworld.js'
-// });
+// SLWORLD front-end
+let promise = minify({
+    compressor: gcc,
+    input: './public/javascripts/*.js',
+    output: './public/javascripts/dist/slworld.js'
+});
 
-// promise.then(function (min) {
-//     console.log(min);
-// });
-// promise.catch(function (err) {
-//     console.log(err);
-// });
+promise.then(function (min) {
+    console.log(min);
+});
+promise.catch(function (err) {
+    console.log(err);
+});
